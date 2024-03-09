5797299fe4bd6e5cf2a5ae7f9c51fe623392bfb9
min zhang <zhangmin1984121@gmail.com>
2024-03-09 13:15:35 -0500

    auto generated code from python scrtipts

---
+++ autogen/version.c
@@ -0,0 +1,4 @@
+const unsigned char fw_version_major_ = 0;
+const unsigned char fw_version_minor_ = 5;
+const unsigned char fw_version_revision_ = 6;
+const unsigned char fw_version_unreleased_ = 1;
